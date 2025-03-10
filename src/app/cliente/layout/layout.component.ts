d8ec9f4fceccdd3bb632cb5c9e454e987447c5bd
alejDev <aseckrr05@gmail.com>
2025-03-10 21:02:57 +0100

    Responsive Login-Registro & Footer

--- src/app/cliente/layout/layout.component.ts
+++ src/app/cliente/layout/layout.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import {HeaderClienteComponent} from '../header-cliente/header-cliente.component';
 import {RouterOutlet} from '@angular/router';
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-layout',
   imports: [
-    HeaderClienteComponent,
-    RouterOutlet
-  ],
+    FooterComponent,
+    RouterOutlet,
+    FooterComponent
+],
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
