ac16aed0bb322d7630cddde9f3d0a1ffef9b311b
alejDev <aseckrr05@gmail.com>
2025-03-11 13:03:57 +0100

    home&Profiles

---
+++ src/app/cliente/profile-client/profile-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-client',
+  imports: [],
+  templateUrl: './profile-client.component.html',
+  styleUrl: './profile-client.component.scss'
+})
+export class ProfileClientComponent {
+
+}
